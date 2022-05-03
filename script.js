70db88191dadb701bc492f428d2896f4a4f53e03
Reza Amraei <amraeireza@gmail.com>
2022-05-03 13:34:21 -0700

    Super Ninja

--- script.js
+++ script.js
@@ -18,6 +18,28 @@ class Ninja {
     return (this.health += 10);
   }
 }
+
+class Sensei extends Ninja {
+  constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10) {
+    super(name, health, speed, strength);
+    this.wisdom = wisdom;
+  }
+  speakWisdom() {
+    console.log(
+      "What one programmer can do in one month, two programmers can do in two months."
+    );
+    super.drinkSake();
+  }
+  showStatsSen() {
+    super.showStats();
+    console.log(`Wisdom: ${this.wisdom}`);
+  }
+}
+
 const Reza = new Ninja("reza", 100, 20);
 Reza.drinkSake();
 Reza.showStats();
+
+const RezaSen = new Sensei("rezaSen", 500);
+RezaSen.speakWisdom();
+RezaSen.showStatsSen();
